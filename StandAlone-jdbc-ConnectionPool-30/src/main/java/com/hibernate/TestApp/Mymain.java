2cc324a64fd2182e2d45591bcaad66fb54ce7f27
cotlin <cotlin@DESKTOP-RNJ3PV0>
2021-07-30 20:03:28 -0700

    standalone connection pool

---
+++ StandAlone-jdbc-ConnectionPool-30/src/main/java/com/hibernate/TestApp/Mymain.java
@@ -0,0 +1,53 @@
+package com.hibernate.TestApp;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import com.hibernate.Entity.Caller_tune;
+
+public class Mymain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Configuration cff = null;
+		SessionFactory sef = null;
+		Session ses = null;
+		boolean flag = false;
+
+		Transaction tx = null;
+		try {
+			cff = new Configuration();
+			cff.configure("com/hibernate/Cfg/hibernate.cfg.xml");
+			sef = cff.buildSessionFactory();
+			Caller_tune cd = new Caller_tune();
+			cd.setLastNm("mehnido");
+			cd.setSimOwner("selesh");
+			cd.setLocation("bhopal");
+			ses = sef.openSession();
+			tx = ses.beginTransaction();
+//			Caller_tune dr=ses.get(Caller_tune.class,2);
+//			dr.setSimOwner("kamas");
+         	ses.save(cd);
+			
+			flag = true;
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println("cougth-exception");
+			e.printStackTrace();
+		}
+		
+		  finally { 
+			  
+			  if(flag==true)
+			  { 
+				  tx.commit();
+		  System.out.println("record inserted"); } else { tx.rollback();
+		  System.out.println("falied"); } }
+		 
+
+	}
+
+}

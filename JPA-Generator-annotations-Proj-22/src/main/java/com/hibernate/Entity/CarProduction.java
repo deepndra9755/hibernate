e4087616f138a941aef9bc3dd9d0417e11048106
cotlin <cotlin@DESKTOP-RNJ3PV0>
2021-08-07 16:58:56 -0700

    JPA genrator Annotation proj 22

---
+++ JPA-Generator-annotations-Proj-22/src/main/java/com/hibernate/Entity/CarProduction.java
@@ -0,0 +1,88 @@
+package com.hibernate.Entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.GenericGenerators;
+import org.hibernate.annotations.Parameter;
+
+@Entity
+public class CarProduction implements Serializable {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	//this is identity generator config
+	
+	
+	Integer id;
+	
+	@Column(name ="gair")
+	Integer gair;
+	
+	@Column(name ="price")
+	Float price;
+	
+	@Column(name ="Cname")
+	String Cname;
+	
+	@Column(name ="Ctype")
+	String Ctype;
+	
+	@Column(name ="average")
+	String average;
+	
+	@Column(name ="company")
+	String company;
+	
+	public Integer getGair() {
+		return gair;
+	}
+	public void setGair(Integer gair) {
+		this.gair = gair;
+	}
+	public Float getPrice() {
+		return price;
+	}
+	public void setPrice(Float price) {
+		this.price = price;
+	}
+	public String getCname() {
+		return Cname;
+	}
+	public void setCname(String cname) {
+		Cname = cname;
+	}
+	public String getCtype() {
+		return Ctype;
+	}
+	public void setCtype(String ctype) {
+		Ctype = ctype;
+	}
+	public String getAverage() {
+		return average;
+	}
+	public void setAverage(String average) {
+		this.average = average;
+	}
+	public String getCompany() {
+		return company;
+	}
+	public void setCompany(String company) {
+		this.company = company;
+	}
+	@Override
+	public String toString() {
+		return "CarProduction [id=" + id + ", gair=" + gair + ", price=" + price + ", Cname=" + Cname + ", Ctype="
+				+ Ctype + ", average=" + average + ", company=" + company + "]";
+	}
+
+	
+	
+
+}
